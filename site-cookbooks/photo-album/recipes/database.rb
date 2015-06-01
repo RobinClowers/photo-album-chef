ac9fe8a5149caa3a581c871a311f7d19a7997f08
Robin Clowers <robin.clowers@gmail.com>
2015-05-31 22:33:21 -0500

    Add backup code

--- site-cookbooks/photo-album/recipes/database.rb
+++ site-cookbooks/photo-album/recipes/database.rb
@@ -19,3 +19,21 @@ postgresql_database_user 'photo_album' do
     password: node['postgresql']['password']['postgres']
   )
 end
+
+backup_install node.name
+backup_generate_config node.name
+package "libxml2-dev"
+package "libxslt1-dev"
+gem_package "fog" do
+  version "~> 1.4.0"
+end
+
+backup_generate_model "pg" do
+  description "backup of postgres"
+  backup_type "database"
+  database_type "PostgreSQL"
+  split_into_chunks_of 2048
+  store_with({"engine" => "S3", "settings" => { "s3.access_key_id" => "sample", "s3.secret_access_key" => "sample", "s3.region" => "us-east-1", "s3.bucket" => "sample", "s3.path" => "/", "s3.keep" => 10 } } ) options({"db.name" => "\"postgres\"", "db.username" => "\"postgres\"", "db.password" => "\"somepassword\"", "db.host" => "\"localhost\"" })
+  mailto "robin.clowers-pg-backup@gmail.com"
+  action :backup
+end
